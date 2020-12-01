1aaafdf26e808813b4b88dbfde2027d8b5b26d8e
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2020-12-01 19:19:27 +0700

    Create models Course, Tag, and Curriculum

--- main/models.py
+++ main/models.py
@@ -4,10 +4,51 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Curriculum(models.Model):
+    """
+    Curriculum of the course.
+    """
+    name = models.CharField(max_length=31)
+    year = models.PositiveSmallIntegerField()
+
+
+class Tag(models.Model):
+    """
+    Tags represents some attributes of course, such as:
+    * Study program that are required to take this course
+        - Ilmu Komputer
+        - Sistem Informasi
+    * Areas of interest
+        - Arsitektur dan Komputasi Awan
+        - Teknologi Perangkat Lunak
+        - Sains Data & Analitika
+        - Kecerdasan Buatan
+        - Tata Kelola SI/TI
+        - E-Bisnis
+        - Ekonomi Digital
+    * Others (?)
+    """
+    name = models.CharField(max_length=31)
+
+
+class Course(models.Model):
+    """
+    Mata Kuliah.
+    """
+    code = models.CharField(max_length=10)
+    name = models.CharField(max_length=127)
+    aliasName = models.CharField(max_length=63)
+    sks = models.PositiveSmallIntegerField()
+    description = models.CharField(max_length=127)
+    prerequisites = models.ManyToManyField("self", symmetrical=False)
+    curriculums = models.ManyToManyField(Curriculum)
+    tags = models.ManyToManyField(Tag)
+
+
 class Profile(models.Model):
-    '''
+    """
     User information generated from SSO UI attributes.
-    '''
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=63)
     npm = models.CharField(max_length=10)
@@ -16,3 +57,4 @@ class Profile(models.Model):
     faculty = models.CharField(max_length=63)
     study_program = models.CharField(max_length=63)
     educational_program = models.CharField(max_length=63)
+    bookmarked_courses = models.ManyToManyField(Course)
