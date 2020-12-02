eb2aa1bbe755e089556e6269efd3b0f1b2146549
Reyhan Alhafizal <reyhanhamidi@gmail.com>
2020-12-02 12:20:12 +0700

    Change login method with decorator
    - Redirect to SSO UI login if user does not authenticated in SSO UI
    - Login endpoint didn't ask any input

--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
-from main.utils import process_sso_profile
-from sso.utils import authenticate, get_cas_client
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from datetime import datetime
+from .utils import process_sso_profile
+from sso.decorators import with_sso_ui
+
 # Create your views here.
 
 
@@ -20,27 +21,19 @@ def sample_api(request):
 
 @api_view(['GET', 'POST'])
 @permission_classes((permissions.AllowAny,))
-def login(request):
+@with_sso_ui()
+def login(request, sso_profile):
     """
     Verify SSO UI ticket and service_url.
     Create a new user & profile if it doesn't exists
     and return token if ticket is valid.
     """
-    # TODO: Testing with actual frontend
-    data = request.POST
-    ticket = data.get("ticket")
-    service_url = data.get("service_url")
-    if (ticket is not None) and (service_url is not None):
-        client = get_cas_client(service_url)
-        sso_profile = authenticate(ticket, client)
-        if sso_profile is not None:
-            token = process_sso_profile(sso_profile)
-            data = {'token': token, 'profile': sso_profile}
-            return Response(data=data)
+    if sso_profile is not None:
+        token = process_sso_profile(sso_profile)
+        data = {'token': token, 'profile': sso_profile}
+        return Response(data=data)
 
-    data = {'message': 'invalid ticket',
-            'received_ticket': ticket,
-            'received_service_url': service_url, }
+    data = {'message': 'invalid sso'}
     return Response(data=data, status=status.HTTP_401_UNAUTHORIZED)
 
 
