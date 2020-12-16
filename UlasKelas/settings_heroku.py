d30da07f03f0ffa2fa1ec01ce77c44ac8416fb3f
Michael Halim <halimmichael09@gmail.com>
2020-12-16 13:48:23 +0700

    ix setting CD

--- UlasKelas/settings_heroku.py
+++ UlasKelas/settings_heroku.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from corsheaders import defaults
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
