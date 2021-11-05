bba2eed5872d77abe5288cdaeb0730aee6810c14
Kevin <kevindharmawan2002@gmail.com>
2021-11-05 19:20:04 +0700

    Added query count decorator

---
+++ main/decorators.py
@@ -0,0 +1,26 @@
+from django.db import connection, reset_queries
+
+import time
+import functools
+
+def query_count(func):
+
+    @functools.wraps(func)
+    def inner_func(*args, **kwargs):
+
+        reset_queries()
+        
+        start_queries = len(connection.queries)
+
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        end = time.perf_counter()
+
+        end_queries = len(connection.queries)
+
+        print(f"Function : {func.__name__}")
+        print(f"Number of Queries : {end_queries - start_queries}")
+        print(f"Finished in : {(end - start):.2f}s")
+        return result
+
+    return inner_func
