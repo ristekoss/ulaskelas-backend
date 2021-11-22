3db8260272a5dcc1736cbeb1fe77ae2fd5008e2c
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-11-22 11:06:45 +0700

    chore: update allowedhost

--- UlasKelas/settings.py
+++ UlasKelas/settings.py
@@ -36,7 +36,7 @@ DEBUG = True
 # DEBUG = False
 
 # TODO: Input prodcution URL
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '172.31.30.38']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
