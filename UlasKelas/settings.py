80fe27595f37bbadd4e7fd0c4aeb62053aced5ea
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-11-22 11:31:07 +0700

    chore: update staticfile root

--- UlasKelas/settings.py
+++ UlasKelas/settings.py
@@ -168,7 +168,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 CORS_ALLOW_HEADERS = list(defaults.default_headers) + [
     "Access-Control-Expose-Headers",
