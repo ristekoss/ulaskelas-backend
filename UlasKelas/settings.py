6a6ee031935a1741911b19967a18b60404bc9df1
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-11-30 08:48:15 +0700

    chore: fix collect static

--- UlasKelas/settings.py
+++ UlasKelas/settings.py
@@ -164,11 +164,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 CORS_ALLOW_HEADERS = list(defaults.default_headers) + [
     "Access-Control-Expose-Headers",
