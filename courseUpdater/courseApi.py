c4ea388b928c49be765717aad3fad293593a6dd7
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-12-02 20:00:52 +0700

    feat: update sunjad base url

--- courseUpdater/courseApi.py
+++ courseUpdater/courseApi.py
@@ -12,8 +12,7 @@ def update_courses():
             course.save()
 
 def _get_courses_json():
-    url = 'https://3e081de5-8b4c-46ea-8736-99476c47204b.mock.pstmn.io/courses'
-    # url = django_settings.SUNJAD_BASE_URL + 'susunjadwal/api/courses'
+    url = django_settings.SUNJAD_BASE_URL + 'susunjadwal/api/courses'
 
     r = requests.get(url)
 
