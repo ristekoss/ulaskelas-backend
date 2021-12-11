0c785c829a1ef59e6c117b756270084452a658e0
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-12-11 07:29:08 +0700

    chore: allow cors

--- main/apps.py
+++ main/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
-
 class MainConfig(AppConfig):
     name = 'main'
+
+    def ready(self):
+        # Makes sure all signal handlers are connected
+        from main import handlers  # noqa
