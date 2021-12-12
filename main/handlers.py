393e99acca78060f57e7e494e42751426952f696
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2021-12-12 12:36:41 +0700

    fix: cors

--- main/handlers.py
+++ main/handlers.py
@@ -2,7 +2,7 @@ from corsheaders.signals import check_request_enabled
 
 
 def cors_allow_api_to_everyone(sender, request, **kwargs):
-    return request.path.startswith("/api/")
+    return request.path.startswith("/")
 
 
 check_request_enabled.connect(cors_allow_api_to_everyone)
