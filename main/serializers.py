2a2ff350e950df0b664c9a5ee6c6c0d00e852b3f
Muhammad Erlangga <intern.erlangga@sayurbox.com>
2022-01-03 21:44:44 +0700

    feat: add info couse

--- main/serializers.py
+++ main/serializers.py
@@ -29,6 +29,7 @@ class CourseSerializer(serializers.ModelSerializer):
     # curriculums = CurriculumSerializer(read_only=True, many=True)
     review_count = serializers.IntegerField()
     code_desc = serializers.SerializerMethodField('get_code_desc')
+    tags = serializers.SerializerMethodField('get_top_tags')
     
     def get_code_desc(self, obj):
         course_prefixes = get_config('course_prefixes')
@@ -36,14 +37,38 @@ class CourseSerializer(serializers.ModelSerializer):
         if code in course_prefixes:
             return course_prefixes[code]
         return None
+    
+    def get_top_tags(self, obj):
+        tag_count = {}
+
+        for review in obj.reviews.all():
+            for review_tag in review.review_tags.all():
+                tag_name = review_tag.tag.tag_name
+
+                if tag_name in tag_count:
+                    tag_count[tag_name] += 1
+                else:
+                    tag_count[tag_name] = 1
+
+        top_tags = [k for k, v in sorted(tag_count.items(), key=lambda tag: tag[1], reverse=True)]
+        return top_tags[:3]
 
     class Meta:
         model = Course
-        fields = ('code', 'code_desc', 'curriculum', 'name', 'description', 'sks', 'term', 'prerequisites', 'review_count')
+        fields = [field.name for field in model._meta.fields]
+        fields.extend(['review_count','code_desc', 'tags'])
 
 class CourseDetailSerializer(serializers.ModelSerializer):
     review_count = serializers.SerializerMethodField('get_review_count')
     tags = serializers.SerializerMethodField('get_top_tags')
+    code_desc = serializers.SerializerMethodField('get_code_desc')
+
+    def get_code_desc(self, obj):
+        course_prefixes = get_config('course_prefixes')
+        code = obj.code[:4]
+        if code in course_prefixes:
+            return course_prefixes[code]
+        return None
 
     def get_review_count(self, obj):
         return obj.reviews.count()
@@ -65,7 +90,8 @@ class CourseDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('code', 'curriculum', 'name', 'description', 'sks', 'term', 'prerequisites', 'review_count', 'tags')
+        fields = [field.name for field in model._meta.fields]
+        fields.extend(['review_count','code_desc', 'tags'])
 
 class ReviewSerializer(serializers.ModelSerializer):
     likes_by = serializers.SerializerMethodField('get_likes')
@@ -77,9 +103,8 @@ class ReviewSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Review
-        fields = ('id', 'author', 'author_generation', 'author_study_program',
-        'course_code','created_at','updated_at','academic_year',
-        'semester','content','hate_speech_status','sentimen','is_anonym', 'tags', 'likes_by')
+        fields = [field.name for field in model._meta.fields]
+        fields.extend(['author', 'author_generation', 'author_study_program', 'course_code', 'tags', 'likes_by'])
 
     def get_author(self, obj):
         return obj.user.username
@@ -123,7 +148,7 @@ class ReviewDSSerializer(serializers.ModelSerializer):
 
 class BookmarkSerializer(serializers.ModelSerializer):
     user = serializers.SerializerMethodField('get_user')
-    course_code = serializers.SerializerMethodField('get_course')
+    course_code = serializers.SerializerMethodField('get_course_code')
     course_code_desc = serializers.SerializerMethodField('get_course_code_desc')
     course_name = serializers.SerializerMethodField('get_course_name')
     course_review_count = serializers.SerializerMethodField('get_course_review_count')
@@ -140,7 +165,7 @@ class BookmarkSerializer(serializers.ModelSerializer):
     
     def get_course_code_desc(self, obj):
         course_prefixes = get_config('course_prefixes')
-        code = obj.code[:4]
+        code = obj.course.code[:4]
         if code in course_prefixes:
             return course_prefixes[code]
         return None
