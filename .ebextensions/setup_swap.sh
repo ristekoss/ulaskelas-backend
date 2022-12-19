92a551e02717a08004c1a02ff23f8de80d24a880
Widyanto Nugroho <widyantohadi@gmail.com>
2022-12-19 07:27:36 +0700

    add setup swap and setup https with certbot

---
+++ .ebextensions/setup_swap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SWAPFILE=/var/swapfile
+SWAP_MEGABYTES=768
+
+if [ -f $SWAPFILE ]; then
+	echo "Swapfile $SWAPFILE found, assuming already setup"
+	exit;
+fi
+
+/bin/dd if=/dev/zero of=$SWAPFILE bs=1M count=$SWAP_MEGABYTES
+/bin/chmod 600 $SWAPFILE
+/sbin/mkswap $SWAPFILE
+/sbin/swapon $SWAPFILE
