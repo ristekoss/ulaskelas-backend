cc8ffcbca383b5f02096f67e63a5b31293d5230a
Widyanto Nugroho <widyantohadi@gmail.com>
2022-12-29 14:49:39 +0700

    add default value for debug

--- UlasKelas/settings.py
+++ UlasKelas/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", 'false').lower() == 'true'
+DEBUG = env("DEBUG").lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
