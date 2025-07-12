6ded13c77cf48cd25b3ac796b44b60086e541b97
Irfan Kamil <96167730+IrPanK@users.noreply.github.com>
2025-07-12 20:14:57 +0700

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # Export env variables
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' .env)
 
 python manage.py collectstatic --no-input && python manage.py migrate && gunicorn --bind 0.0.0.0:${PORT} --workers 2 UlasKelas.wsgi
